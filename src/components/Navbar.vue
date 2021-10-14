53fe6d17c567e034164d9fcb5c9db50f38b52ae3
GBstallman <nduwimanaguymich@gmail.com>
2021-10-14 09:44:48 -0700

    created Navbar components and modify home page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div>
+     <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Guy<span>Michel</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  created() {
+
+  },
+  data() {
+    return {
+    }
+  },
+  props: {
+   
+  },
+  methods: {
+   
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
